caf8d90521494eb2136e2dc213d41a27b87889b6
Mohammed Zaloom <125036726+mozaloom@users.noreply.github.com>
2023-09-25 15:27:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ profile_pic = current_dir / "assets" / "profile-pic.png"
 
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Portfolio | Mohammed Zaloom"
-PAGE_ICON = current_dir / "assets" / "profile-pic (19).png"
+PAGE_ICON = "💼"
 NAME = "Mohammed Zaloom"
 DESCRIPTION = """
 Artificial Intelligence and Robotics student at Al- Balqa' Applied University (BAU), passion for learning. I aim to keep learning and become a better version of myself every day.
